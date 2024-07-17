2d7b7cb7dd9783cfa4e55a492dbf359ae84c2263
pieceowater <pieceowater@gmail.com>
2024-07-17 17:55:08 +0500

    configs + gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigAppModule } from './config/config.module';
-import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigAppModule, RabbitmqModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
