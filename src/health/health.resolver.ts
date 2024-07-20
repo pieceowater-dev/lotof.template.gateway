a59a9d4c421cadafad6b336393c42a0e589ba8af
pieceowater <pieceowater@gmail.com>
2024-07-20 16:36:39 +0500

    lib test

--- src/health/health.resolver.ts
+++ src/health/health.resolver.ts
@@ -1,10 +1,12 @@
 // src/health/health.resolver.ts
 import { Resolver, Query } from '@nestjs/graphql';
+import { greet } from '@pieceowater-dev/lotof.lib.broadcaster';
 
 @Resolver()
 export class HealthResolver {
   @Query(() => String)
   health(): string {
+    console.log(greet('pieceowater'));
     return 'OK';
   }
 }
