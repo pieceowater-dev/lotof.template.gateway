658829415a630ff246c2d628e7ceef38dc4e0119
pieceowater <pieceowater@gmail.com>
2024-07-21 23:08:26 +0500

    meh

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ConfigService } from '@nestjs/config'
-import { Transport, MicroserviceOptions } from '@nestjs/microservices'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService)
-  const rabbitMQUrl = configService.get<string>('RABBITMQ_URL')
-  const port = configService.get<number>('PORT') || 3000
+  const configService = app.get(ConfigService);
+  // noinspection TypeScriptValidateTypes
+  const rabbitMQUrl = configService.get<string>('RABBITMQ_URL');
+  // noinspection TypeScriptValidateTypes
+  const port = configService.get<number>('PORT') || 3000;
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
@@ -19,10 +21,10 @@ async function bootstrap() {
         durable: false,
       },
     },
-  })
+  });
 
-  await app.startAllMicroservices()
-  await app.listen(port)
-  console.log(`Application is running on: ${await app.getUrl()}`)
+  await app.startAllMicroservices();
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap()
+bootstrap();
