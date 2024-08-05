f5f70b75835e97e6ce581d61033074745b998487
pieceowater <pieceowater@gmail.com>
2024-08-05 11:47:41 +0500

    validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // check to config -> https://docs.nestjs.com/techniques/validation
+    } as ValidationPipeOptions),
+  );
 
   const configService = app.get(ConfigService);
 
